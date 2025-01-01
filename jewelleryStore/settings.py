6e2dcb1e047969fc4b104c0ed2361447390dcfb9
Utkarsh3042 <u.saraswat30@gmail.com>
2025-01-02 00:14:07 +0530

    settings changed

--- jewelleryStore/settings.py
+++ jewelleryStore/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bz)%zs#n0r_45w1u)og$wo51#j!^1i)u(^^lo)rco_txu6k97n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
