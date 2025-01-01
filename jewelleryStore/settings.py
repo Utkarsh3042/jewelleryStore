96cea7616d0d2c815d89dbd91caff72a126acbfd
Utkarsh3042 <u.saraswat30@gmail.com>
2025-01-02 01:10:05 +0530

    settings fix

--- jewelleryStore/settings.py
+++ jewelleryStore/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bz)%zs#n0r_45w1u)og$wo51#j!^1i)u(^^lo)rco_txu6k97n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0','govindamgems.onrender.com']
 
